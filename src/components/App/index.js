f0933dbc0f8b53c05c4b0bed6fcf1e053a0af7d9
Sai Ram Somanaboina <sai.somanaboina@myntra.com>
2019-09-21 03:32:02 +0530

    added redux

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import RaisedButton from 'material-ui/RaisedButton';
-import Style from './styles.css'
-const App = props => <div className={Style.container}>
- <RaisedButton label="CLICK ME" />
+import {connect } from 'react-redux';
+const App = props => <div>
+ <RaisedButton label={'Click to increment'} onClick={props.onClick} />
+ {props.counter}
 </div>
-export default App;
+App.propTypes = {
+ counter: PropTypes.number.isRequired,
+ onClick: PropTypes.func.isRequired
+}
+App.defaultProps = {
+ counter: 10,
+ onClick: () => console.log('default click action')
+}
+
+const mapStateToProps = (state, ownProps) => {
+  debugger;
+  return {
+    counter: state.basic.counter
+  }
+}
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+      onClick: () => {
+        dispatch({type: 'INCREMENT'})
+      }
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import "@babel/polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { reducers } from './reducers';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import App from './components/App';
-import './global.css'
+let store = createStore(reducers);
 ReactDOM.render(
-  <MuiThemeProvider>
-    <App />
-  </MuiThemeProvider>,
+ <Provider store={store}>
+    <MuiThemeProvider>
+      <App />
+    </MuiThemeProvider>
+   </Provider>,
   document.getElementById('app')
 );
